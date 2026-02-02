8cddb2b4d7a3043dc0b1f39d9a5efef85b1a12bb
SpectraSynq <spectrasynq@example.com>
2026-02-02 22:19:20 +0800

    chore(p4_audio_producer): add Component Manager deps and hardware config
    Add idf_component.yml (espressif/esp-dsp, espressif/es8311).
    Add ES8311 GPIO config in audio_i2s_config.h (I2S DO/MCK, I2C, PA).
    Enable DSP in sdkconfig; remove CONFIG_DSP_ENABLED from sdkconfig.defaults.
    Ignore build/, .cache/, managed_components/, dependencies.lock, sdkconfig.old.

--- p4_audio_producer/main/audio_i2s_config.h
+++ p4_audio_producer/main/audio_i2s_config.h
@@ -5,10 +5,18 @@
 #ifndef P4_AUDIO_PRODUCER_AUDIO_I2S_CONFIG_H
 #define P4_AUDIO_PRODUCER_AUDIO_I2S_CONFIG_H
 
-#define AUDIO_I2S_PORT_NUM   0
-#define AUDIO_I2S_BCK_GPIO   12
-#define AUDIO_I2S_WS_GPIO    10
-#define AUDIO_I2S_DI_GPIO    11
-#define AUDIO_I2S_MCK_GPIO   (-1)
+#define AUDIO_I2S_PORT_NUM        0
+#define AUDIO_I2S_BCK_GPIO        12
+#define AUDIO_I2S_WS_GPIO         10
+#define AUDIO_I2S_DI_GPIO         11
+#define AUDIO_I2S_DO_GPIO         9
+#define AUDIO_I2S_MCK_GPIO        13
+#define AUDIO_I2S_MCLK_MULTIPLE   384
+
+#define AUDIO_I2C_PORT_NUM        0
+#define AUDIO_I2C_SCL_GPIO        8
+#define AUDIO_I2C_SDA_GPIO        7
+
+#define AUDIO_PA_GPIO             53
 
 #endif /* P4_AUDIO_PRODUCER_AUDIO_I2S_CONFIG_H */
